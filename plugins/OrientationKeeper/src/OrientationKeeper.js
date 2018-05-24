4e415c745dfd314d9235086398c9f4b8103e0e69
Jacob Michelsen <chromegadget@gmail.com>
2018-05-24 16:02:11 +0200

    Working OrientationKeeper, allows for setting starting yaw for 360 scenes, independent of starting gyro value

--- plugins/OrientationKeeper/src/OrientationKeeper.js
+++ plugins/OrientationKeeper/src/OrientationKeeper.js
@@ -41,40 +41,16 @@ ForgePlugins.OrientationKeeper.prototype =
       this.viewer.gyroscope.onDeviceOrientationChange.add(this.gyroHandler, this);
       this.viewer.gyroscope.onScreenOrientationChange.add(this.orientationChangeHandler, this);
 
-      this._camera.onOrientationChange.add(this.cameraChangeHandler, this);
-
       this.gyroHandler();
       this.orientationChangeHandler();
       this.sceneLoadHandler();
     },
 
-    cameraChangeHandler: function () {
-      console.log("Camera yaw is", this._camera.yaw);
-      this._camera.yaw = this._camera.yaw + FORGE.Math.degToRad(this._sceneAdjustmentYaw);
-      console.log("Camera yaw is now", this._camera.yaw);
-    },
-
     update: function () {
-      //console.log(this._camera.yaw);
-      //this._camera.yaw = 20;
       if (FORGE.Device.gyroscope === false) {
         return;
       }
-      // var display = this.viewer.renderer.display;
-      // // Get frame data before pose to ensure pose values are up to date
-      // var frameData = display.vrFrameData;
-      // var quat = display.getQuaternionFromPose();
-
-      // if (quat !== null) {
-      //   this._quaternion = quat;
-      //   this._updateFromQuaternion();
-      // }
-
-
-
       this.viewer.camera.quaternion = this._posQuatFinal;
-
-
     },
 
 
@@ -86,6 +62,8 @@ ForgePlugins.OrientationKeeper.prototype =
       if (FORGE.Device.gyroscope === false) {
         return;
       }
+      this.viewer.gyroscope.onDeviceOrientationChange.remove(this.gyroHandler, this);
+      this.viewer.gyroscope.onScreenOrientationChange.remove(this.orientationChangeHandler, this);
     },
 
     gyroHandler: function (event) {
@@ -104,11 +82,19 @@ ForgePlugins.OrientationKeeper.prototype =
         position = /** @type {DeviceOrientation} */ event.data;
       }
 
-      // Set the position in correct Euler coordinates
       this._posEuler.set(FORGE.Math.degToRad(position.beta), FORGE.Math.degToRad(position.alpha), -FORGE.Math.degToRad(position.gamma), "YXZ");
-      this._posQuatIndermediate.setFromEuler(this._posEuler);
 
-
+      //Adjust for the given scene
+      if (this._sceneAdjustmentQuat !== null) {
+        var quat = new THREE.Quaternion();
+        // Start with an adjusted rotation
+        this._posQuatIndermediate = this._sceneAdjustmentQuat.clone();
+        // Set the position in correct Euler coordinates
+        this._posQuatIndermediate.multiply(quat.setFromEuler(this._posEuler));
+      } else {
+      // Set the position in correct Euler coordinates
+        this._posQuatIndermediate.setFromEuler(this._posEuler);
+      }
 
       // Add the offset provided by the camera
       this._posQuatIndermediate.multiply(this._posQuatOffset);
@@ -117,18 +103,14 @@ ForgePlugins.OrientationKeeper.prototype =
       // Adjust given the screen orientation
       this._posQuatIndermediate.multiply(this._posQuatScreenOrientation);
 
+
       // Final inversion, see FORGE.RenderDisplay#getQuaternionFromPose method
       this._posQuatFinal.set(-this._posQuatIndermediate.y, -this._posQuatIndermediate.x, -this._posQuatIndermediate.z, this._posQuatIndermediate.w);
 
-      //Adjust for the given scene
-      if (this._sceneAdjustmentQuat !== null) {
-        //this._posQuatFinal.multiply(this._sceneAdjustmentQuat);
-        //this._posQuatFinal.multiply(new THREE.Quaternion(-this._sceneAdjustmentQuat.y, -this._sceneAdjustmentQuat.x, -this._sceneAdjustmentQuat.z, this._sceneAdjustmentQuat.w));
-      }
     },
 
     orientationChangeHandler: function (event) {
-      console.log("Current yaw is", this._camera.yaw);
+      //console.log("Current yaw is", this._camera.yaw);
       if (typeof screen.orientation !== "undefined") {
         this._screenOrientation = FORGE.Math.degToRad(screen.orientation.angle);
       }
@@ -143,25 +125,30 @@ ForgePlugins.OrientationKeeper.prototype =
       this._sceneAdjustmentYaw = 0;
       window.yawOffset = 0;
       this._sceneAdjustmentQuat = null;
-      if (!this.viewer.story.scene.config.startingYaw) {
+      if (this.viewer.story.scene.config.startingYaw === undefined) {
         console.log("no target yaw");
         return;
       }
       this._currentTargetYaw = this.viewer.story.scene.config.startingYaw;
       console.log("target yaw is", this._currentTargetYaw);
-      setTimeout(this.changeSceneOrientation.bind(this), 2500);
-      //quaternion = new THREE.Quaternion().setFromAxisAngle(this._yAxis, angleOfRotation );
-      //setTimeout(() => this._camera.lookAt(45),2);
+      setTimeout(this.changeSceneOrientation.bind(this), 50);
     },
 
     changeSceneOrientation: function() {
+      console.log("Disabling gyro");
+      this._gyroscope.enabled = false;
       var currentYaw = this._camera.yaw;
       console.log("yaw at start of scene is", currentYaw);
       this._sceneAdjustmentYaw = this._currentTargetYaw-currentYaw+360;
       window.yawOffset = this._currentTargetYaw-currentYaw + 360;
       this._sceneAdjustmentQuat = new THREE.Quaternion();
-      this._sceneAdjustmentQuat.setFromAxisAngle( new THREE.Vector3( 0, 1, 0 ), FORGE.Math.degToRad(this._currentTargetYaw-currentYaw+360));
+      this._sceneAdjustmentQuat.setFromAxisAngle( new THREE.Vector3( 0, 1, 0 ), FORGE.Math.degToRad(-this._sceneAdjustmentYaw));
       console.log("We are adjusting by", this._currentTargetYaw-currentYaw);
+      setTimeout(this.printNewYaw.bind(this), 1000);
+    },
+
+    printNewYaw: function() {
+      console.log("new yaw is now", this._camera.yaw);
     }
 
   };
